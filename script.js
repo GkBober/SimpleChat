217e6c5017b0e7093353246508797d1fa5ace8de
Bober <szummik@gmail.com>
2025-06-20 21:31:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-let nickname = "";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-function startChat() {
-  const input = document.getElementById("nickname");
-  if (input.value.trim()) {
-    nickname = input.value.trim();
-    document.getElementById("nickname-screen").style.display = "none";
-    document.getElementById("chat-screen").style.display = "flex";
-    document.getElementById("message-input").focus();
-  }
-}
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAtoZXTV0yhT4G5mZAT-4-9EP3Cdv1pqnQ",
+  authDomain: "simplechat-8d876.firebaseapp.com",
+  projectId: "simplechat-8d876",
+  storageBucket: "simplechat-8d876.firebasestorage.app",
+  messagingSenderId: "763791658945",
+  appId: "1:763791658945:web:e5366df7b7c19a1f592940",
+  measurementId: "G-J5LSSBJ50Q"
+};
 
-function sendMessage(event) {
-  if (event.key === "Enter") {
-    const input = event.target;
-    const msg = input.value.trim();
-    if (msg) {
-      const chatBox = document.getElementById("chat-box");
-      const messageElement = document.createElement("div");
-      messageElement.textContent = `<${nickname}>> ${msg}`;
-      chatBox.appendChild(messageElement);
-      chatBox.scrollTop = chatBox.scrollHeight;
-      input.value = "";
-    }
-  }
-}
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
